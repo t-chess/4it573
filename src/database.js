e802b6fa8f14af24e29032e54d49fa9055266030
Tatiana Chess <t.chess@outlook.com>
2023-06-22 19:59:53 +0200

    cmon

--- src/database.js
+++ src/database.js
@@ -1,4 +1,10 @@
 import knex from "knex";
 import knexfile from "../knexfile.js";
 
-export const db = knex(knexfile.development);
+let environment = "development";
+
+if (process.env.NODE_ENV === "production") {
+  environment = "production";
+}
+
+export const db = knex(knexfile[environment]);
